7f08841d0694d789067efb2ddea45d66bd6e3ace
LONGRUYUN <longruyun111@gmail.com>
2016-11-18 19:25:23 -0800

    Create connector.h

---
+++ src/connector.h
@@ -0,0 +1,46 @@
+
+
+#ifndef connector_h
+#define connector_h
+
+#include "analyse.h"
+#include <iostream>
+#include <string>
+
+
+class Connector{
+private:
+    string type;
+    
+public:
+    Connector() {}
+    
+    Connector(string conn) {
+        this->type = conn;
+    }
+    
+    // return which the connector is
+    int whichConnector() {
+        if(type == "||") return 1;
+        
+        if(type == "&&") return 2;
+        
+        if(type == ";") return 3;
+        
+        return -1;
+    }
+    
+    string getType() {
+        return type;
+    }
+    
+    friend ostream& operator <<(ostream& os, Connector c) {
+        os << c.getType();
+        
+        return os;
+    }
+    
+};
+
+
+#endif /* connector_h */
