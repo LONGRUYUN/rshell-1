7a7bcde1d9216233bb0789fe2957736a7684811a
LONGRUYUN <longruyun111@gmail.com>
2016-11-18 19:30:20 -0800

    Create multi_command.sh

---
+++ tests/multi_command.sh
@@ -0,0 +1,7 @@
+ls -a; echo hello && mkdir test || echo world; git status
+echo a || echo hello
+ls -a; echo hello; mkdir test
+echo hello || mkdir test
+echo hello && mkdir test
+ech o && echo a
+echo hello && 
